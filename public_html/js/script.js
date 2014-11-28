3d1e2ca82a64590805c045e6456cd1123174378c
Ilya V. Guy <ilya@ilyaguy.me>
2014-11-28 17:06:51 +0200

    check unique value

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -6,6 +6,7 @@
 var Solver = function () {
     this.data;
     this.flag;
+    this.tempData;
 };
 
 /**
@@ -16,26 +17,31 @@ var Solver = function () {
  */
 Solver.prototype.solve = function (data) {
     this.data = data;
-    var tempData = [];
+    this.tempData = [];
     this.flag = false;
+
     for (var x = 0; x <= 8; x++) {
-        tempData[x] = [];
+        this.tempData[x] = [];
         for (var y = 0; y <= 8; y++) {
-            var p = this.preparePossible(x, y);
-            if (typeof p !== 'number') {
-                if (p.length == 1) {
-                    p = p[0];
-                    this.flag = true;
-                } else {
-                    p = p.join('');
-                }
-            }
-            tempData[x][y] = p;
+            this.tempData[x][y] = this.preparePossible(x, y);
         }
     }
-    console.table(tempData);
+
+    for (var x = 0; x <= 8; x++) {
+        for (var y = 0; y <= 8; y++) {
+	    if (typeof this.tempData[x][y] == 'object' && this.tempData[x][y].length > 1) {
+		for (t = 0; t < this.tempData[x][y].length; t++) {
+		    if (this.checkUnique(x, y, this.tempData[x][y][t]) == 1) {
+			this.tempData[x][y] = this.tempData[x][y][t];
+		    }
+		}
+	    }
+        }
+    }
+
+    console.table(this.tempData);
     console.log(this.flag);
-    return tempData;
+    return this.tempData;
 };
 
 
@@ -47,10 +53,11 @@ Solver.prototype.solve = function (data) {
  * @returns {integer|Array}
  */
 Solver.prototype.preparePossible = function (x, y) {
-    var currentValue = (typeof this.data[x][y] == 'string') ? 0 : this.data[x][y];
+    var currentValue = (typeof this.data[x][y] != 'number') ? 0 : this.data[x][y];
     if (currentValue != 0) {
         return currentValue;
     }
+
     currentArray = [];
     for (p = 1; p < 10; p++) {
         var square = this.checkSquare(x, y, p);
@@ -58,9 +65,18 @@ Solver.prototype.preparePossible = function (x, y) {
         var post = this.checkColumn(x, y, p);
         currentArray[p] = this.merge(square, line, post);
     }
+
     currentArray = currentArray.filter(function (e) {
         return e != 0
     });
+
+    if (currentArray.length == 1) {
+        this.flag = true;
+        return currentArray[0];
+    } else {
+//	currentArray = currentArray.join('');
+    }
+
     return currentArray;
 };
 
@@ -125,14 +141,40 @@ Solver.prototype.merge = function (a, b, c) {
 };
 
 Solver.prototype.prettyPrint = function (data) {
-    var res = '<table style="border: 1px solid cyan;">';
+    var res = '<table style="border: 1px solid black; margin-top: 15px;">';
+    var cellStyleDefault = 'border: 1px dotted grey;';
+    var cellStyleRow = 'border-bottom: 2px solid black;';
+    var cellStyleColumn = 'border-right: 2px solid black;';
     for (i = 0; i <= 8; i++) {
         res += '<tr>';
+	var rclass = (i%3==2) ? cellStyleRow : '';
         for (j = 0; j <= 8; j++) {
-            res += '<td style="border: 1px dotted grey;">' + data[i][j] + '</td>';
+	    var cclass = (j%3==2) ? cellStyleColumn : '';
+	    res += '<td style="text-align: center; width: 60px; border: 1px dotted grey; ' + rclass + cclass+ '">' + data[i][j] + '</td>';
         }
         res += '</tr>';
     }
     res += '</table>';
     return res;
 };
+
+Solver.prototype.checkUnique = function (x, y, v) {
+    // check unique value in current square
+    // 0, 3, 6
+    var sx = x - x % 3, ex = sx + 3, sy = y - y % 3, ey = sy + 3;
+    var counter = 0;
+    for (i = sx; i < ex; i++) {
+        for (j = sy; j < ey; j++) {
+            if (typeof this.tempData[i][j] == 'object') {
+		if (this.tempData[i][j].indexOf(v) != -1) {
+		    counter++;
+		}
+            } else {
+		if (this.tempData[i][j] == v) {
+		    counter++;
+		}
+	    }
+        }
+    }
+    return counter;
+}
