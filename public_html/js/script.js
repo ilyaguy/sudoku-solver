93c83c34bd3dd507392c8f3d6e7c81af31ab38e0
Ilya V. Guy <ilya.daemon@gmail.com>
2014-11-28 23:16:55 +0200

    Style, loop, global variables.

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -17,30 +17,32 @@ var Solver = function () {
  */
 Solver.prototype.solve = function (data) {
     this.data = data;
-    this.tempData = [];
-    this.flag = false;
+    do {
+        this.tempData = [];
+        this.flag = false;
 
-    for (var x = 0; x <= 8; x++) {
-        this.tempData[x] = [];
-        for (var y = 0; y <= 8; y++) {
-            this.tempData[x][y] = this.preparePossible(x, y);
+        for (var x = 0; x <= 8; x++) {
+            this.tempData[x] = [];
+            for (var y = 0; y <= 8; y++) {
+                this.tempData[x][y] = this.preparePossible(x, y);
+            }
         }
-    }
 
-    for (var x = 0; x <= 8; x++) {
-        for (var y = 0; y <= 8; y++) {
-	    if (typeof this.tempData[x][y] == 'object' && this.tempData[x][y].length > 1) {
-		for (t = 0; t < this.tempData[x][y].length; t++) {
-		    if (this.checkUnique(x, y, this.tempData[x][y][t]) == 1) {
-			this.tempData[x][y] = this.tempData[x][y][t];
-		    }
-		}
-	    }
+        for (var x = 0; x <= 8; x++) {
+            for (var y = 0; y <= 8; y++) {
+                if (typeof this.tempData[x][y] == 'object' && this.tempData[x][y].length > 1) {
+                    for (var t = 0; t < this.tempData[x][y].length; t++) {
+                        if (this.checkUnique(x, y, this.tempData[x][y][t]) == 1) {
+                            this.tempData[x][y] = this.tempData[x][y][t];
+                        }
+                    }
+                }
+            }
         }
-    }
 
-    console.table(this.tempData);
-    console.log(this.flag);
+        console.table(this.tempData);
+        this.data = this.tempData;
+    } while (this.flag == true);
     return this.tempData;
 };
 
@@ -58,8 +60,8 @@ Solver.prototype.preparePossible = function (x, y) {
         return currentValue;
     }
 
-    currentArray = [];
-    for (p = 1; p < 10; p++) {
+    var currentArray = [];
+    for (var p = 1; p < 10; p++) {
         var square = this.checkSquare(x, y, p);
         var line = this.checkRow(x, y, p);
         var post = this.checkColumn(x, y, p);
@@ -91,8 +93,8 @@ Solver.prototype.preparePossible = function (x, y) {
 Solver.prototype.checkSquare = function (x, y, p) {
     // 0, 3, 6
     var sx = x - x % 3, ex = sx + 3, sy = y - y % 3, ey = sy + 3;
-    for (i = sx; i < ex; i++) {
-        for (j = sy; j < ey; j++) {
+    for (var i = sx; i < ex; i++) {
+        for (var j = sy; j < ey; j++) {
             if (this.data[i][j] == p) {
                 return 0;
             }
@@ -110,7 +112,7 @@ Solver.prototype.checkSquare = function (x, y, p) {
  * @returns {integer}
  */
 Solver.prototype.checkRow = function (x, y, p) {
-    for (j = 0; j <= 8; j++) {
+    for (var j = 0; j <= 8; j++) {
         if (this.data[x][j] == p) {
             return 0;
         }
@@ -127,7 +129,7 @@ Solver.prototype.checkRow = function (x, y, p) {
  * @returns {integer}
  */
 Solver.prototype.checkColumn = function (x, y, p) {
-    for (i = 0; i <= 8; i++) {
+    for (var i = 0; i <= 8; i++) {
         if (this.data[i][y] == p) {
             return 0;
         }
@@ -145,12 +147,18 @@ Solver.prototype.prettyPrint = function (data) {
     var cellStyleDefault = 'border: 1px dotted grey;';
     var cellStyleRow = 'border-bottom: 2px solid black;';
     var cellStyleColumn = 'border-right: 2px solid black;';
-    for (i = 0; i <= 8; i++) {
+    for (var i = 0; i <= 8; i++) {
         res += '<tr>';
-	var rclass = (i%3==2) ? cellStyleRow : '';
-        for (j = 0; j <= 8; j++) {
-	    var cclass = (j%3==2) ? cellStyleColumn : '';
-	    res += '<td style="text-align: center; width: 60px; border: 1px dotted grey; ' + rclass + cclass+ '">' + data[i][j] + '</td>';
+        var rclass = (i % 3 == 2) ? cellStyleRow : '';
+        for (var j = 0; j <= 8; j++) {
+            var cclass = (j % 3 == 2) ? cellStyleColumn : '';
+            var optag = '', cltag = '';
+            if (data[i][j] != this.data[i][j]) {
+                optag = '<b>';
+                cltag = '</b>';
+            }
+            res += '<td style="text-align: center; width: 60px; border: 1px dotted grey; ' + rclass + cclass + '">'
+                    + optag + data[i][j] + cltag + '</td>';
         }
         res += '</tr>';
     }
@@ -163,17 +171,17 @@ Solver.prototype.checkUnique = function (x, y, v) {
     // 0, 3, 6
     var sx = x - x % 3, ex = sx + 3, sy = y - y % 3, ey = sy + 3;
     var counter = 0;
-    for (i = sx; i < ex; i++) {
-        for (j = sy; j < ey; j++) {
+    for (var i = sx; i < ex; i++) {
+        for (var j = sy; j < ey; j++) {
             if (typeof this.tempData[i][j] == 'object') {
-		if (this.tempData[i][j].indexOf(v) != -1) {
-		    counter++;
-		}
+                if (this.tempData[i][j].indexOf(v) != -1) {
+                    counter++;
+                }
             } else {
-		if (this.tempData[i][j] == v) {
-		    counter++;
-		}
-	    }
+                if (this.tempData[i][j] == v) {
+                    counter++;
+                }
+            }
         }
     }
     return counter;
